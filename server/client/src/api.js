4ef56307c5927dbcc7487130a9f6d1ba8cd8944c
Abhinav Chaudhary <67009976+abhinavch1234@users.noreply.github.com>
2026-05-02 21:41:36 +0530

    Update api.js

--- server/client/src/api.js
+++ server/client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const API = axios.create({ baseURL: 'https://project-production.up.railway.app/api' });
+const API = axios.create({ baseURL: 'https://project-production-126f.up.railway.app' });
 
 export const login = (formData) => API.post('/auth/login', formData);
 export const register = (formData) => API.post('/auth/register', formData);
