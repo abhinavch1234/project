e2d7259faae31f48e3852b68829a8686379aa600
Abhinav Chaudhary <67009976+abhinavch1234@users.noreply.github.com>
2026-05-02 21:38:24 +0530

    Change API base URL to production environment

--- server/client/src/api.js
+++ server/client/src/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+
+const API = axios.create({ baseURL: 'https://project-production.up.railway.app/api' });
 
 export const login = (formData) => API.post('/auth/login', formData);
 export const register = (formData) => API.post('/auth/register', formData);
